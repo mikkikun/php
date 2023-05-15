b904fe6cfcd17a6263a8e40d79420a2472454235
mikkikun <nattukun.1111@icloud.com>
2023-05-15 22:12:54 +0900

    ‘test’

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,8 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
+
+mix.browserSync({
+    proxy: 'http://localhost/',
+    open: false
+});
