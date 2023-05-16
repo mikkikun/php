5a72d00eb5f90c384e7fa2ab699ff90ab8bb0cba
mikkikun <nattukun.1111@icloud.com>
2023-05-16 17:40:52 +0900

    ‘spa切り替え’

--- routes/web.php
+++ routes/web.php
@@ -20,59 +20,60 @@ use App\Http\Controllers\Admin\MapController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::view('/', 'welcome');
+Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
+// Route::view('/', 'welcome');
 
-Auth::routes();
-Route::get('/guest-login', [ LoginController::class, 'guest'])->name('guestLogin');
+// Auth::routes();
+// Route::get('/guest-login', [ LoginController::class, 'guest'])->name('guestLogin');
 
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('post/create', [PostController::class, 'add'])->middleware('auth')->name('create');
-    Route::post('post/create', [PostController::class, 'create'])->middleware('auth');
-    Route::get('post/edit', [PostController::class, 'edit'])->middleware('auth'); 
-    Route::post('post/edit', [PostController::class, 'update'])->middleware('auth');
-    Route::post('post/index', [PostController::class, 'delete'])->middleware('auth');
-    Route::get('post/index', [PostController::class, 'index'])->middleware('auth')->name('top'); 
-    Route::get('post/follow_pose', [PostController::class, 'follow_pose'])->middleware('auth')->name('follow_pose');
-    Route::post('favorites', [FavoritesController::class, 'store'])->middleware('auth')->name('favorites');
-    Route::delete('favorites', [FavoritesController::class, 'destroy'])->middleware('auth')->name('unfavorites');
-    Route::get('post/favorite', [FavoritesController::class, 'favorite_page'])->middleware('auth');
-    Route::get('comment/favorite', [FavoritesController::class, 'comment_favorite_page'])->middleware('auth');
-    Route::get('replie/favorite', [FavoritesController::class, 'replie_favorite_page'])->middleware('auth');
+// Route::group(['prefix' => 'admin'], function() {
+//     Route::get('post/create', [PostController::class, 'add'])->middleware('auth')->name('create');
+//     Route::post('post/create', [PostController::class, 'create'])->middleware('auth');
+//     Route::get('post/edit', [PostController::class, 'edit'])->middleware('auth'); 
+//     Route::post('post/edit', [PostController::class, 'update'])->middleware('auth');
+//     Route::post('post/index', [PostController::class, 'delete'])->middleware('auth');
+//     Route::get('post/index', [PostController::class, 'index'])->middleware('auth')->name('top'); 
+//     Route::get('post/follow_pose', [PostController::class, 'follow_pose'])->middleware('auth')->name('follow_pose');
+//     Route::post('favorites', [FavoritesController::class, 'store'])->middleware('auth')->name('favorites');
+//     Route::delete('favorites', [FavoritesController::class, 'destroy'])->middleware('auth')->name('unfavorites');
+//     Route::get('post/favorite', [FavoritesController::class, 'favorite_page'])->middleware('auth');
+//     Route::get('comment/favorite', [FavoritesController::class, 'comment_favorite_page'])->middleware('auth');
+//     Route::get('replie/favorite', [FavoritesController::class, 'replie_favorite_page'])->middleware('auth');
     
 
-    Route::get('profile/edit', [ProfileController::class, 'edit'])->middleware('auth')->name('profile-edit'); 
-    Route::put('profile/edit', [ProfileController::class, 'update'])->middleware('auth');
-    Route::get('profile/delte', [ProfileController::class, 'deletepage'])->middleware('auth')->name('profile-delete-page');
-    Route::post('profile/delte', [ProfileController::class, 'delete'])->middleware('auth');
-    Route::get('profile/userpage', [ProfileController::class, 'userpage'])->middleware('auth')->name('mypage'); 
+//     Route::get('profile/edit', [ProfileController::class, 'edit'])->middleware('auth')->name('profile-edit'); 
+//     Route::put('profile/edit', [ProfileController::class, 'update'])->middleware('auth');
+//     Route::get('profile/delte', [ProfileController::class, 'deletepage'])->middleware('auth')->name('profile-delete-page');
+//     Route::post('profile/delte', [ProfileController::class, 'delete'])->middleware('auth');
+//     Route::get('profile/userpage', [ProfileController::class, 'userpage'])->middleware('auth')->name('mypage'); 
 
-    Route::post('profile/userpage', [ProfileController::class, 'follow'])->middleware('auth')->name('follow');
-    Route::delete('profile/userpage',[ProfileController::class, 'unfollow'])->middleware('auth')->name('unfollow');
-    Route::get('profile/follow', [ProfileController::class, 'follow_page'])->middleware('auth'); 
-    Route::get('profile/follower', [ProfileController::class, 'follower_page'])->middleware('auth'); 
+//     Route::post('profile/userpage', [ProfileController::class, 'follow'])->middleware('auth')->name('follow');
+//     Route::delete('profile/userpage',[ProfileController::class, 'unfollow'])->middleware('auth')->name('unfollow');
+//     Route::get('profile/follow', [ProfileController::class, 'follow_page'])->middleware('auth'); 
+//     Route::get('profile/follower', [ProfileController::class, 'follower_page'])->middleware('auth'); 
 
-    Route::get('comment/index', [CommentsController::class, 'index'])->middleware('auth'); 
-    Route::post('comment/index', [CommentsController::class, 'create'])->middleware('auth');
-    Route::get('comment/edit', [CommentsController::class, 'edit'])->middleware('auth'); 
-    Route::post('comment/edit', [CommentsController::class, 'update'])->middleware('auth');
-    Route::delete('comment/index', [CommentsController::class, 'delete'])->middleware('auth');
-    Route::get('comment/replie', [CommentsController::class, 'replie'])->middleware('auth'); 
-    Route::post('comment/replie', [CommentsController::class, 'replie_create'])->middleware('auth');
-    Route::get('comment/replie_edit', [CommentsController::class, 'replie_edit'])->middleware('auth'); 
-    Route::post('comment/replie_edit', [CommentsController::class, 'replie_update'])->middleware('auth');
+//     Route::get('comment/index', [CommentsController::class, 'index'])->middleware('auth'); 
+//     Route::post('comment/index', [CommentsController::class, 'create'])->middleware('auth');
+//     Route::get('comment/edit', [CommentsController::class, 'edit'])->middleware('auth'); 
+//     Route::post('comment/edit', [CommentsController::class, 'update'])->middleware('auth');
+//     Route::delete('comment/index', [CommentsController::class, 'delete'])->middleware('auth');
+//     Route::get('comment/replie', [CommentsController::class, 'replie'])->middleware('auth'); 
+//     Route::post('comment/replie', [CommentsController::class, 'replie_create'])->middleware('auth');
+//     Route::get('comment/replie_edit', [CommentsController::class, 'replie_edit'])->middleware('auth'); 
+//     Route::post('comment/replie_edit', [CommentsController::class, 'replie_update'])->middleware('auth');
     
-    Route::post('favorites_comment', [FavoritesController::class, 'comment_store'])->middleware('auth')->name('favorites_comment');
-    Route::delete('favorites_comment', [FavoritesController::class, 'comment_destroy'])->middleware('auth')->name('unfavorites_comment');
-    Route::post('favorites_replie', [FavoritesController::class, 'replie_store'])->middleware('auth')->name('favorites_replie');
-    Route::delete('favorites_replie', [FavoritesController::class, 'replie_destroy'])->middleware('auth')->name('unfavorites_replie');
+//     Route::post('favorites_comment', [FavoritesController::class, 'comment_store'])->middleware('auth')->name('favorites_comment');
+//     Route::delete('favorites_comment', [FavoritesController::class, 'comment_destroy'])->middleware('auth')->name('unfavorites_comment');
+//     Route::post('favorites_replie', [FavoritesController::class, 'replie_store'])->middleware('auth')->name('favorites_replie');
+//     Route::delete('favorites_replie', [FavoritesController::class, 'replie_destroy'])->middleware('auth')->name('unfavorites_replie');
 
-    Route::get('chat/chat', [ChatController::class, 'index'])->middleware('auth');
-    Route::post('chat', [ChatController::class, 'add'])->middleware('auth');
-    Route::delete('chat', [ChatController::class, 'delete'])->middleware('auth');
-    Route::get('chat/list', [ChatController::class, 'list'])->middleware('auth')->name('chat-list');
+//     Route::get('chat/chat', [ChatController::class, 'index'])->middleware('auth');
+//     Route::post('chat', [ChatController::class, 'add'])->middleware('auth');
+//     Route::delete('chat', [ChatController::class, 'delete'])->middleware('auth');
+//     Route::get('chat/list', [ChatController::class, 'list'])->middleware('auth')->name('chat-list');
 
-    Route::get('map/map', [MapController::class, 'map'])->middleware('auth')->name('map');
-    Route::get('map/currentLocation', [MapController::class, 'currentLocation'])->middleware('auth')->name('result.currentLocation');
-});
+//     Route::get('map/map', [MapController::class, 'map'])->middleware('auth')->name('map');
+//     Route::get('map/currentLocation', [MapController::class, 'currentLocation'])->middleware('auth')->name('result.currentLocation');
+// });
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,9 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js').vue() 
     .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+    .version();
 
 mix.browserSync({
     proxy: 'http://localhost/',
